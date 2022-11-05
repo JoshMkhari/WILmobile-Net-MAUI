a54eba6156e697a8112ade38628041a6ab6e9e04
unknown <njmkhari@gmail.com>
2022-11-05 10:54:22 +0200

    Somewhat happy with progress

--- PointOfSale/Models/AppData.cs
+++ PointOfSale/Models/AppData.cs
@@ -3,21 +3,21 @@ namespace PointOfSale.Models;
 public static class AppData
 {
     private static Random random = new Random();
-    public static string[] Statuses = new string[] { "Ready to Pay", "Cooking", "Ready to Order" };
+    public static string[] Statuses = new string[] { "Cat", "Dog","Cat", "Dog","Cat", "Dog","Cat", "Dog","Cat", "Cat"};
     public static List<int> Tables = new List<int> { 7,8,9,10,11,12,13,14 };
 
     public static List<Item> Items = new List<Item>
     {
-        new Item(){ Title = "Japchae", Price    = 13.99, Quantity = 1, Image = "japchae.png", Category = ItemCategory.Noodles},
-        new Item(){ Title = "Jajangmyeon", Price = 14.99, Quantity = 1, Image = "jajangmyeon.png", Category = ItemCategory.Noodles},
-        new Item(){ Title = "Janchi Guksu", Price = 12.99, Quantity = 1, Image = "janchi_guksu.png", Category = ItemCategory.Noodles},
-        new Item(){ Title = "Budae Jjigae", Price = 14.99, Quantity = 1, Image = "budae_jjigae.png", Category = ItemCategory.Noodles},
-        new Item(){ Title = "Naengmyeon", Price = 12.99, Quantity = 1, Image = "naengmyeon.png", Category = ItemCategory.Noodles},
-        new Item(){ Title = "Soda", Price = 2.50, Quantity = 1, Category = ItemCategory.Beverages, Image = "soda.png"},
-        new Item(){ Title = "Iced Tea", Price = 3.50, Quantity = 1, Category = ItemCategory.Beverages, Image = "iced_tea.png"},
-        new Item(){ Title = "Hot Tea", Price = 4.00, Quantity = 1, Category = ItemCategory.Beverages, Image = "tea.png"},
-        new Item(){ Title = "Coffee", Price = 4.00, Quantity = 1, Category = ItemCategory.Beverages, Image = "coffee.png"},
-        new Item(){ Title = "Milk", Price = 5.00, Quantity = 1, Category = ItemCategory.Beverages, Image = "milk.png"},
+        new Item(){ Title = "Burmese Cat", Price    = 13.99, Quantity = 1, Image = "japchae.png", Category = ItemCategory.Noodles},
+        new Item(){ Title = "Dachshund Dog", Price = 14.99, Quantity = 1, Image = "jajangmyeon.png", Category = ItemCategory.Noodles},
+        new Item(){ Title = "Chartreux Cat", Price = 12.99, Quantity = 1, Image = "janchi_guksu.png", Category = ItemCategory.Noodles},
+        new Item(){ Title = "Coton de Tulear Dog", Price = 14.99, Quantity = 1, Image = "budae_jjigae.png", Category = ItemCategory.Noodles},
+        new Item(){ Title = "Scottish Fold Cat", Price = 12.99, Quantity = 1, Image = "naengmyeon.png", Category = ItemCategory.Noodles},
+        new Item(){ Title = "Belgian Shepherd Dog", Price = 2.50, Quantity = 1, Category = ItemCategory.Beverages, Image = "soda.png"},
+        new Item(){ Title = "Siamese Cat", Price = 3.50, Quantity = 1, Category = ItemCategory.Beverages, Image = "iced_tea.png"},
+        new Item(){ Title = "Curly Coated Retriever ", Price = 4.00, Quantity = 1, Category = ItemCategory.Beverages, Image = "tea.png"},
+        new Item(){ Title = "Toyger Cat", Price = 4.00, Quantity = 1, Category = ItemCategory.Beverages, Image = "coffee.png"},
+        new Item(){ Title = "Abyssinian Cat", Price = 5.00, Quantity = 1, Category = ItemCategory.Beverages, Image = "milk.png"},
     };
     
     public static List<Order> Orders { get; set; } = GenerateOrders();
@@ -31,8 +31,8 @@ public static class AppData
             orders.Add((new Order()
             {
                 Table = Tables[i],
-                Status = RandomStatus(),
-                Items = GenerateItems()
+                Status = RandomStatus(i),
+                Items = GenerateItems(i)
             }));
         }
 
@@ -40,22 +40,16 @@ public static class AppData
         return orders;
     }
 
-    private static List<Item> GenerateItems()
+    private static List<Item> GenerateItems(int i)
     {
-        List<Item> items = new List<Item>(); 
-        int numItems = random.Next(1, Items.Count - 1);
-        random.Shuffle(Items);
-        for (int i = 0; i < numItems; i++)
-        {
-            items.Add(Items[i]);
-        }
-
+        List<Item> items = new List<Item>();
+        items.Add(Items[i]);
         return items;
     }
 
-    private static string RandomStatus()
+    private static string RandomStatus(int i)
     {
-        var i = random.Next(0, Statuses.Length - 1);
+        //var i = random.Next(0, Statuses.Length - 1);
         return Statuses[i];
     }
 }
